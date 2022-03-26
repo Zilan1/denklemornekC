1442da3ada509ac4a3de6f50d06d5f4e696df446
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 15:44:32 +0300

    sum

---
+++ proje70.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	//for(i=0;i<10;i++)
+	//{
+		//if(i==5)
+		//continue;/break;
+	//}
+	//printf("%d",i);
+
+	
+	
+	
+	
+	
+	
+	//for(int i=0;i<5;i++)
+	//{
+		//for(int j=0;j<5;j++)
+		//{
+			//printf("%d*%d=%d\t",i,j,i*j);
+		//}
+	//}
+	
+	
+	return 0;
+}
